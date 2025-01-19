d485562fa4a6ab42e5eb31089cadbc870bdbb9f0
lokpro <2010297+lokpro@users.noreply.github.com>
2025-01-20 00:30:41 +0800

    fix @updateURL, @downloadURL

--- portal-Images.meta.js
+++ portal-Images.meta.js
@@ -7,7 +7,7 @@
 // @id             portal-images
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
 // @updateURL      https://github.com/Ingrass/IITC-portal-Images/raw/master/Scripts/IITC-portal-Images.meta.js
-// @downloadURL    https://github.com/Ingrass/IITC-portal-Images/raw/master/Scripts/IITC-portal-Images.user.js
+// @downloadURL    https://raw.githubusercontent.com/Ingrass/IITC-portal-Images/refs/heads/main/portal-Images.user.js
 // @match          https://intel.ingress.com/*
 // @match          https://intel-x.ingress.com/*
 // @grant          none
--- portal-Images.user.js
+++ portal-Images.user.js
@@ -6,8 +6,8 @@
 // @description    Show portal images on the map.
 // @id             portal-images
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
-// @updateURL      https://github.com/Ingrass/IITC-portal-Images/raw/master/Scripts/IITC-portal-Images.meta.js
-// @downloadURL    https://github.com/Ingrass/IITC-portal-Images/raw/master/Scripts/IITC-portal-Images.user.js
+// @updateURL      https://raw.githubusercontent.com/Ingrass/IITC-portal-Images/refs/heads/main/portal-Images.meta.js
+// @downloadURL    https://raw.githubusercontent.com/Ingrass/IITC-portal-Images/refs/heads/main/portal-Images.user.js
 // @match          https://intel.ingress.com/*
 // @match          https://intel-x.ingress.com/*
 // @grant          none
